79658e0a7ad78045c846e332489afbec917a3c36
wanlizhuiyun <wanlizhuiyun@users.noreply.github.com>
2015-11-14 16:11:47 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,18 +6,18 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
-echo '***************************************'
-echo '*                                     *'
-echo '*   Fresh Server Initialization!      *'
-echo '*                                     *'
-echo '***************************************'
-
-#Add repositories
-sudo yum -y install yum-plugin-priorities epel-release http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el7.rf.x86_64.rpm
+echo '***********************************************'
+echo '*                                             *'
+echo '*        Fresh Server Initialization!         *'
+echo '*                                             *'
+echo '***********************************************'
 
 #Configure repositories' priority
+sudo yum -y install yum-plugin-priorities
 sudo sed -i -e "s/\]$/\]\npriority=1/g" /etc/yum.repos.d/CentOS-Base.repo
+sudo yum -y install epel-release
 sudo sed -i -e "s/\]$/\]\npriority=5/g" /etc/yum.repos.d/epel.repo
+sudo yum -y install http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el7.rf.x86_64.rpm
 sudo sed -i -e "s/\]$/\]\npriority=10/g" /etc/yum.repos.d/rpmforge.repo
 
 #Update whole system
