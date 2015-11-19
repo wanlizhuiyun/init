72579895c504a25ebea7894123df40cbe8016214
wanlizhuiyun <shisen.buaa@gmail.com>
2015-11-19 14:53:31 +0800

    add lemp

---
+++ lemp/lemp.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#Program:
+#This program setup LAMP infrastructure.
+#History:
+#2015/11/13	Ghost from Beihang University	First release
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+#Install lamp
+yum -y install httpd mariadb-server mariadb php php-mysql mod_ssl
+
+#Configure lamp
+setsebool -P httpd_unified 1
+#firewall-cmd --permanent --zone=public --add-service=http
+firewall-cmd --permanent --zone=public --add-service=https
+firewall-cmd --reload
+systemctl start httpd
+systemctl start mariadb
+systemctl enable httpd
+systemctl enable mariadb
+mysql_secure_installation
+
+#ssl
+mkdir /etc/httpd/ssl
+openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout /etc/httpd/ssl/apache.key -out /etc/httpd/ssl/apache.crt
+#must configure /etc/httpd/conf.d/ssl.conf
+
+#Setup virtual host directory structure
+mkdir /etc/httpd/sites-available
+mkdir /etc/httpd/sites-enabled
+cat >> /etc/httpd/conf/httpd.conf << EOF
+IncludeOptional sites-enabled/*.conf
+EOF
+
+#Finish
+chmod -R 755 /var/www
+systemctl restart httpd
---
+++ lemp/newhost_nginx.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#Program:
+#This program setup LAMP infrastructure.
+#History:
+#2015/11/13	Ghost from Beihang University	First release
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+read -p "Enter ServerName: " servername
+echo ""
+read -p "Enter ServerAlias: " serveralias
+echo ""
+
+#Create web directory structure
+mkdir -p /var/www/${servername}/public_html
+touch /var/www/${servername}/public_html/index.html
+cat > /var/www/${servername}/public_html/index.html << EOF
+<html>
+	<head>
+		<title>Sorry</title>
+	</head>
+	<body>
+		<h1>Site in construction, coming soon...</h1>
+	</body>
+</html>
+EOF
+chown -R apache:apache /var/www/${servername}/public_html
+chmod -R 755 /var/www/${servername}
+
+#Setup virtual host
+touch /etc/httpd/sites-available/${servername}.conf
+cat > /etc/httpd/sites-available/${servername}.conf << EOF
+<VirtualHost *:433>
+	ServerName ${servername}
+	ServerAlias ${serveralias}
+	DocumentRoot /var/www/${servername}/public_html
+	ErrorLog logs/${servername}-error_log
+	CustomLog logs/${servername}-access_log combined
+	LogLevel warn
+	SSLEngine on
+	SSLProtocol all -SSLv2
+	SSLCipherSuite HIGH:MEDIUM:!aNULL:!MD5
+	SSLCertificateFile /etc/httpd/ssl/apache.crt
+	SSLCertificateKeyFile /etc/httpd/ssl/apache.key
+	<Files ~ "\.(cgi|shtml|phtml|php3?)$">
+		SSLOptions +StdEnvVars
+	</Files>
+	<Directory "/var/www/cgi-bin">
+		SSLOptions +StdEnvVars
+	</Directory>
+	BrowserMatch "MSIE [2-5]" \
+		nokeepalive ssl-unclean-shutdown \
+		downgrade-1.0 force-response-1.0
+</VirtualHost>
+EOF
+ln -s /etc/httpd/sites-available/${servername}.conf /etc/httpd/sites-enabled/${servername}.conf
+
+#Finish
+systemctl restart httpd
