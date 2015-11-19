06bf7be8e9e3db2e8db14eb094be89aab384e571
wanlizhuiyun <wanlizhuiyun@users.noreply.github.com>
2015-11-19 15:46:14 +0800

    Update lemp.sh

--- lemp/lemp.sh
+++ lemp/lemp.sh
@@ -1,37 +1,39 @@
 #!/bin/bash
 #Program:
-#This program setup LAMP infrastructure.
+#This program setup LEMP infrastructure.
 #History:
-#2015/11/13	Ghost from Beihang University	First release
+#2015/11/19	Ghost from Beihang University	First release
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
 #Install lamp
-yum -y install httpd mariadb-server mariadb php php-mysql mod_ssl
+yum -y install nginx mariadb-server mariadb php php-mysql php-fpm
+nginxpath=/etc/nginx
+docpath=/usr/share/nginx
 
 #Configure lamp
 setsebool -P httpd_unified 1
-#firewall-cmd --permanent --zone=public --add-service=http
+firewall-cmd --permanent --zone=public --add-service=http
 firewall-cmd --permanent --zone=public --add-service=https
 firewall-cmd --reload
-systemctl start httpd
+systemctl start nginx
 systemctl start mariadb
-systemctl enable httpd
+systemctl enable nginx
 systemctl enable mariadb
 mysql_secure_installation
 
 #ssl
-mkdir /etc/httpd/ssl
-openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout /etc/httpd/ssl/apache.key -out /etc/httpd/ssl/apache.crt
-#must configure /etc/httpd/conf.d/ssl.conf
+mkdir ${nginxpath}/ssl
+openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout ${nginxpath}/ssl/nginx.key -out ${nginxpath}/ssl/nginx.crt
+#must configure ${nginxpath}/conf.d/ssl.conf
 
 #Setup virtual host directory structure
-mkdir /etc/httpd/sites-available
-mkdir /etc/httpd/sites-enabled
-cat >> /etc/httpd/conf/httpd.conf << EOF
-IncludeOptional sites-enabled/*.conf
-EOF
+#mkdir ${nginxpath}/sites-available
+#mkdir ${nginxpath}/sites-enabled
+#cat >> ${nginxpath}/conf/httpd.conf << EOF
+#IncludeOptional sites-enabled/*.conf
+#EOF
 
 #Finish
-chmod -R 755 /var/www
-systemctl restart httpd
+chmod -R 755 ${docpath}
+systemctl restart nginx
