841f11384590a4249b2315b16cf825a324509e08
wanlizhuiyun <wanlizhuiyun@users.noreply.github.com>
2015-11-20 16:08:40 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,41 +13,41 @@ echo '*                                             *'
 echo '***********************************************'
 
 #Enable firewalld
-sudo systemctl start firewalld
-sudo systemctl enable firewalld
+systemctl start firewalld
+systemctl enable firewalld
 
 #Set timezone
-sudo timedatectl set-timezone Asia/Shanghai
+timedatectl set-timezone Asia/Shanghai
 
 #Set swap
-sudo fallocate -l 1G /swapfile
-sudo chmod 600 /swapfile
-sudo mkswap /swapfile
-sudo swapon /swapfile
-sudo cat >> /etc/fstab << EOF
+fallocate -l 1G /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+cat >> /etc/fstab << EOF
 /swapfile swap  swap  sw  0 0
 EOF
-sudo sysctl vm.swappiness=10
-sudo sysctl vm.vfs_cache_pressure=50
-sudo cat >> /etc/sysctl.conf << EOF
+sysctl vm.swappiness=10
+sysctl vm.vfs_cache_pressure=50
+cat >> /etc/sysctl.conf << EOF
 vm.swappiness = 10
 vm.vfs_cache_pressure = 50
 EOF
 
 
 #Configure repositories' priority
-sudo yum -y install yum-plugin-priorities
-sudo sed -i -e "s/\]$/\]\npriority=1/g" /etc/yum.repos.d/CentOS-Base.repo
-sudo yum -y install epel-release
-sudo sed -i -e "s/\]$/\]\npriority=5/g" /etc/yum.repos.d/epel.repo
-sudo yum -y install http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el7.rf.x86_64.rpm
-sudo sed -i -e "s/\]$/\]\npriority=10/g" /etc/yum.repos.d/rpmforge.repo
+yum -y install yum-plugin-priorities
+sed -i "s/\]$/\]\npriority=1/g" /etc/yum.repos.d/CentOS-Base.repo
+yum -y install epel-release
+sed -i "s/\]$/\]\npriority=5/g" /etc/yum.repos.d/epel.repo
+yum -y install http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el7.rf.x86_64.rpm
+sed -i "s/\]$/\]\npriority=10/g" /etc/yum.repos.d/rpmforge.repo
 
 #Update whole system
-sudo yum -y update
+yum -y update
 
 #Install tools
-sudo yum -y install wget unzip bzip2 vim
+yum -y install wget unzip bzip2 vim
 
 #Create vim link
 ln -s ~/init/.vim/vimrc ~/.vimrc
