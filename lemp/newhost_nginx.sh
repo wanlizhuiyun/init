cdba9e571c7b3eddbc0b2d021795c4d6866099cc
wanlizhuiyun <wanlizhuiyun@users.noreply.github.com>
2015-11-20 12:14:41 +0800

    Update newhost_nginx.sh

--- lemp/newhost_nginx.sh
+++ lemp/newhost_nginx.sh
@@ -2,15 +2,14 @@
 #Program:
 #This program setup LAMP infrastructure.
 #History:
-#2015/11/13	Ghost from Beihang University	First release
+#2015/11/13     Ghost from Beihang University   First release
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
+nginxpath=/etc/nginx
 htmlpath=/usr/share/nginx
 
 read -p "Enter ServerName: " servername
-echo ""
-read -p "Enter ServerAlias: " serveralias
 echo ""
 
 #Create web directory structure
@@ -18,44 +17,46 @@ mkdir -p ${htmlpath}/${servername}/public_html
 touch ${htmlpath}/${servername}/public_html/index.html
 cat > ${htmlpath}/${servername}/public_html/index.html << EOF
 <html>
-	<head>
-		<title>${servername}</title>
-	</head>
-	<body>
-		<h1>${servername} in construction, coming soon...</h1>
-	</body>
+        <head>
+                <title>${servername}</title>
+        </head>
+        <body>
+                <h1>${servername} in construction, coming soon...</h1>
+        </body>
 </html>
 EOF
-chown -R apache:apache ${htmlpath}/${servername}/public_html
+chown -R nginx:nginx ${htmlpath}/${servername}/public_html
 chmod -R 755 ${htmlpath}/${servername}
 
 #Setup virtual host
-touch /etc/httpd/sites-available/${servername}.conf
-cat > /etc/httpd/sites-available/${servername}.conf << EOF
-<VirtualHost *:433>
-	ServerName ${servername}
-	ServerAlias ${serveralias}
-	DocumentRoot /var/www/${servername}/public_html
-	ErrorLog logs/${servername}-error_log
-	CustomLog logs/${servername}-access_log combined
-	LogLevel warn
-	SSLEngine on
-	SSLProtocol all -SSLv2
-	SSLCipherSuite HIGH:MEDIUM:!aNULL:!MD5
-	SSLCertificateFile /etc/httpd/ssl/apache.crt
-	SSLCertificateKeyFile /etc/httpd/ssl/apache.key
-	<Files ~ "\.(cgi|shtml|phtml|php3?)$">
-		SSLOptions +StdEnvVars
-	</Files>
-	<Directory "/var/www/cgi-bin">
-		SSLOptions +StdEnvVars
-	</Directory>
-	BrowserMatch "MSIE [2-5]" \
-		nokeepalive ssl-unclean-shutdown \
-		downgrade-1.0 force-response-1.0
-</VirtualHost>
+touch ${nginxpath}/sites-available/${servername}.conf
+cat > ${nginxpath}/sites-available/${servername}.conf << EOF
+server {
+        listen  443;
+        server_name     ${servername};
+        root            ${htmlpath}/${servername}/public_html;
+        index           index.php index.html index.htm;
+
+#       location / {
+#               try_files $uri $uri/ =404;
+#       }
+#       error_page 404 /404.html
+#       error_page 500 502 503 504 /50x.html;
+#       location = /50x.html {
+#               root    ${htmlpath}/html;
+#       }
+
+        location ~ \.php$ {
+                try_files \$uri =404;
+                fastcgi_pass unix:/var/run/php-fpm/php-fpm.sock;
+                fastcgi_index index.php;
+                fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+                include fastcgi_params;
+        }
+
+}
 EOF
-ln -s /etc/httpd/sites-available/${servername}.conf /etc/httpd/sites-enabled/${servername}.conf
+ln -s ${nginxpath}/sites-available/${servername}.conf ${nginxpath}/sites-enabled/${servername}.conf
 
 #Finish
-systemctl restart httpd
+systemctl restart nginx
