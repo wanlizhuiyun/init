b5a65f8395d4ae2186aa674f43d641f60c991ac3
wanlizhuiyun <wanlizhuiyun@users.noreply.github.com>
2015-11-20 11:03:07 +0800

    Update lemp.sh

--- lemp/lemp.sh
+++ lemp/lemp.sh
@@ -9,7 +9,7 @@ export PATH
 #Install lamp
 yum -y install nginx mariadb-server mariadb php php-mysql php-fpm
 nginxpath=/etc/nginx
-docpath=/usr/share/nginx
+htmlpath=/usr/share/nginx
 
 #Configure lamp
 #setsebool -P httpd_unified 1
@@ -22,18 +22,34 @@ systemctl enable nginx
 systemctl enable mariadb
 mysql_secure_installation
 
+#config php php-fpm
+cp /etc/php.ini /etc/php.ini.bak
+sed -i "s/\;cgi\.fix\_pathinfo\=1/cgi\.fix\_pathinfo\=0/g" /etc/php.ini
+cp /etc/php-fpm.d/www.conf /etc/php-fpm.d/www.conf.bak
+sed -i "s/^listen\ \=\ 127\.0\.0\.1\:9000$/listen\ \=\ \/var\/run\/php\-fpm\/php\-fpm\.sock/g" /etc/php-fpm.d/www.conf
+sed -i "s/^;listen\.owner\ \=\ nobody$/listen\.owner\ \=\ nobody/g" /etc/php-fpm.d/www.conf
+sed -i "s/^;listen\.group\ \=\ nobody$/listen\.group\ \=\ nobody/g" /etc/php-fpm.d/www.conf
+sed -i "s/^user\ \=\ apache$/user\ \=\ nginx/g" /etc/php-fpm.d/www.conf
+sed -i "s/^group\ \=\ apache$/group\ \=\ nginx/g" /etc/php-fpm.d/www.conf
+systemctl start php-fpm
+systemctl enable php-fpm
+
 #ssl
 mkdir ${nginxpath}/ssl
 openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout ${nginxpath}/ssl/nginx.key -out ${nginxpath}/ssl/nginx.crt
 #must configure ${nginxpath}/conf.d/ssl.conf
 
 #Setup virtual host directory structure
-#mkdir ${nginxpath}/sites-available
-#mkdir ${nginxpath}/sites-enabled
-#cat >> ${nginxpath}/conf/httpd.conf << EOF
-#IncludeOptional sites-enabled/*.conf
-#EOF
+mkdir ${nginxpath}/sites-available
+mkdir ${nginxpath}/sites-enabled
+cp ${nginxpath}/nginx.conf ${nginxpath}/nginx.conf.bak
+cat >> ${nginxpath}/nginx.conf << EOF
+include /etc/nginx/sites-enabled/*.conf;
+EOF
+
+#Virtual Host
+#touch ${nginxpath}/
 
 #Finish
-chmod -R 755 ${docpath}
+chmod -R 755 ${htmlpath}
 systemctl restart nginx
